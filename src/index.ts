2f5df80dcc6eb91f60d9c81cc59c0bb7d8da105e
ghostflyby <ghostflyby+git@outlook.com>
2025-04-07 18:31:00 +0800

    use standard dom api instead of GM

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import css from 'asciinema-player/dist/bundle/asciinema-player.css?raw'
 
 let elements = document.querySelectorAll('article.markdown-body a[href^="https://asciinema.org/a/"]:has(img)') as NodeListOf<HTMLAnchorElement>
 
-GM.addStyle(css)
-
 console.log('Found elements:', elements.length)
 
 import { create } from 'asciinema-player'
@@ -16,13 +14,16 @@ elements.forEach(async (a) => {
 		url.search = ''
 		const target = url + '.cast'
 
-		const player = document.createElement('div')
 		const parent = a.parentElement
 		if (parent === null) return
 		parent.removeChild(a)
+
 		const shadowRoot = parent.attachShadow({ mode: 'open' })
-		shadowRoot.appendChild(player)
-		console.log(target)
+
+		const player = document.createElement('div')
+		const style = document.createElement('style')
+		style.textContent = css
+		shadowRoot.append(style, player)
 		create(target, player)
 	} catch (error) {
 		console.error('Error fetching cast file:', error)
