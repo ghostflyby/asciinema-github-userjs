2ca3f455903b7027f4d828c85ce1bb156de474e6
ghostflyby <ghostflyby+git@outlook.com>
2025-04-08 18:36:41 +0800

    mutationObserver and dirty flag for frontend navigation

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,71 @@
 import playerCss from 'asciinema-player/dist/bundle/asciinema-player.css?raw'
-
-let elements = document.querySelectorAll('article.markdown-body a[href^="https://asciinema.org/a/"]:has(img)') as NodeListOf<HTMLAnchorElement>
-
-console.log('Found elements:', elements.length)
-
 import { create } from 'asciinema-player'
-elements.forEach(async (a) => {
-
-	console.log('Found element:', a.href)
-
-	try {
-		const url = new URL(a.href)
-		url.search = ''
-		const target = url + '.cast'
-		const iframeUrl = url + '/iframe'
-
-		const parent = a.parentElement
-		if (parent === null) return
 
 
+let dirty = true
 
-		console.log('Fetching cast opts:', iframeUrl)
-		GM_xmlhttpRequest({
-			url: iframeUrl, method: 'GET',
-			onload: (response) => {
-				console.log('Fetched cast opts:', iframeUrl)
+setInterval(() => {
+	if (dirty)
+		run()
+	dirty = false
+}, 500)
 
-				const opts = JSON.parse(response.responseText.match(/const opts = (\{.*?\});/s)?.[1] ?? '{}')
-				console.log(opts)
-				parent.removeChild(a)
-				const shadowRoot = parent.attachShadow({ mode: 'open' })
+function run() {
+	let elements = document.querySelectorAll('article.markdown-body a[href^="https://asciinema.org/a/"]:has(img)') as NodeListOf<HTMLAnchorElement>
 
-				const player = document.createElement('div')
-				const playerStyle = document.createElement('style')
-				const hostStyle = document.createElement('style')
-				hostStyle.textContent = `
+	console.log('Found elements:', elements.length)
+
+	elements.forEach(async (a) => {
+
+		console.log('Found element:', a.href)
+
+		try {
+			const url = new URL(a.href)
+			url.search = ''
+			const target = url + '.cast'
+			const iframeUrl = url + '/iframe'
+
+			const parent = a.parentElement
+			if (parent === null) return
+
+
+
+			console.log('Fetching cast opts:', iframeUrl)
+			GM_xmlhttpRequest({
+				url: iframeUrl, method: 'GET',
+				onload: (response) => {
+					console.log('Fetched cast opts:', iframeUrl)
+
+					const opts = JSON.parse(response.responseText.match(/const opts = (\{.*?\});/s)?.[1] ?? '{}')
+					console.log(opts)
+					parent.removeChild(a)
+					const shadowRoot = parent.attachShadow({ mode: 'open' })
+
+					const player = document.createElement('div')
+					const playerStyle = document.createElement('style')
+					const hostStyle = document.createElement('style')
+					hostStyle.textContent = `
 					:host {
 					position: relative;
 					z-index: 0;
 					`
-				playerStyle.textContent = playerCss
-				shadowRoot.append(hostStyle, playerStyle, player)
-				create(target, player, { ...opts })
-			}
-		})
+					playerStyle.textContent = playerCss
+					shadowRoot.append(hostStyle, playerStyle, player)
+					create(target, player, { ...opts })
+				}
+			})
 
-	} catch (error) {
-		console.error('Error fetching cast file:', error)
-	}
+		} catch (error) {
+			console.error('Error fetching cast file:', error)
+		}
+	})
+
+}
+
+const observer = new MutationObserver(() => {
+	dirty = true
+});
+observer.observe(document.body, {
+	childList: true,
+	subtree: true
 })
